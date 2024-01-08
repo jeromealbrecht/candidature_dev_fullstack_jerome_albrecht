0a71d3a19827b0e932e7e83fc9da7722f2d2be18
December81 <megastudio@outlook.fr>
2024-01-08 23:27:26 +0100

    changes CSS

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
 import random
 from flask import Flask, render_template, jsonify
+from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
+basic_auth = BasicAuth(app)
+
+# Utilisateur et mot de passe pour l'authentification de base
+app.config['BASIC_AUTH_USERNAME'] = 'LettreJerome'
+app.config['BASIC_AUTH_PASSWORD'] = 'Lj$8dPc7ùR'
 
 # Liste de tours magiques
 tours_magiques = [
@@ -15,34 +21,15 @@ tours_magiques = [
 ]
 
 @app.route('/')
+@basic_auth.required  # Décorateur pour exiger l'authentification sur cette route
 def index():
     return render_template('index.html')
 
 @app.route('/api/generer_tour_magique', methods=['GET'])
+@basic_auth.required  # Décorateur pour exiger l'authentification sur cette route
 def generer_tour_magique():
     tour_magique = random.choice(tours_magiques)
     return jsonify({'tour_magique': tour_magique})
 
 if __name__ == '__main__':
-    # Utilisons Gunicorn pour le déploiement en production
-    from gunicorn.app.base import BaseApplication
-
-    class GunicornApp(BaseApplication):
-        def __init__(self, app, options=None):
-            self.options = options or {}
-            self.application = app
-            super().__init__()
-
-        def load_config(self):
-            for key, value in self.options.items():
-                self.cfg.set(key, value)
-
-        def load(self):
-            return self.application
-
-    options = {
-        'bind': '0.0.0.0:8080',
-        'workers': 4
-    }
-
-    GunicornApp(app, options).run()
+    app.run(debug=True)
